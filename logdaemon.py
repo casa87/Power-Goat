ef219e3232be2953cfbc65923df1a521b70fbf6e
Christophe <chris@dotcloud.com>
2012-06-06 17:28:37 -0700

    define structure

---
+++ logdaemon.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+log daemon to push log file on Amazon S3
+and rotate them
+
+Christopphe Boudet 2012
+"""
+
+import sys
+
+class Logger():
+    
+    def __init__(self, mode, delay):
+        print mode
+
+def usage(exit=False):
+    print
+    print 'Log daemon'
+    print 
+    print '{0} : '.format(sys.argv[0])
+    print 'size 100M        rotate every 100Mo'
+    print 'date 10d        rotate every 10 days'
+    print
+    if exit:
+        sys.exit(1)
+        
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        usage(True)
+    logger = Logger(sys.argv[1], sys.argv[2])
