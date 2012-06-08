f8d2539ef490cabe99e165c7f98988cec907b00d
Christophe <chris@dotcloud.com>
2012-06-07 17:07:44 -0700

    add rotation of N files

--- logdaemon.py
+++ logdaemon.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-log daemon to push log file on Amazon S3
+log daemon to push log files on Amazon S3
 and rotate them
 
 Christopphe Boudet 2012
@@ -12,8 +12,9 @@ import sys
 import time
 import datetime
 
+
 class Logger():
-    
+
     def __init__(self, mode, delay, files):
         if mode in ('date', 'd'):
             self.mode = 'date'
@@ -24,7 +25,6 @@ class Logger():
         else:
             usage(True)
         self.files = files
-        
 
     def sizeof_fmt(self, num):
         """transform human size into int"""
@@ -40,7 +40,7 @@ class Logger():
                 else:
                     usage(True)
         return num
-            
+
     def sizeof_date(self, num):
         """transform date diff to date"""
         date_letter = ['m', 'h', 'd', 'w']
@@ -51,13 +51,12 @@ class Logger():
                     index = date_letter.index(str(num)[-1])
                     return int(num[:-1]) * date_convertion[index] * 60
         usage(True)
-        
+
     def get_file_to_rotate(self, files):
         """return list of file to rotate"""
         rotate = []
         for file in files:
             if os.path.isfile(file):
-                
                 if self.mode == 'size':
                     if os.path.getsize(file) >= self.delay:
                         rotate.append(file)
@@ -65,21 +64,41 @@ class Logger():
                     created = os.path.getctime(file)
                     now = time.time()
                     if now - created >= self.delay:
-                        rotate.append(file) 
+                        rotate.append(file)
             else:
                 print "{0} is not a file".format(file)
-    
+        return rotate
+
     def check_file(self):
         """"check file to rotate"""
         self.file_rotate = self.get_file_to_rotate(self.files)
-    
+        if not self.file_rotate:
+            print "no rotation"
+            sys.exit()
+
     def rotate(self, max_copy):
         """rotate file"""
-        if self.file_rotate:
-            for file in self.file_rotate:
-                print os.path.dirname(file), file
-        else:
-            print "no rotation" 
+        max_copy = int(max_copy)
+        for file in self.file_rotate:
+            i = max_copy
+            name = os.path.basename(file)
+            path = os.path.dirname(file)
+            #rotate older files
+            while i:
+                file_name = "{0}{1}".format(name, i)
+                if os.path.exists(os.path.join(path, file_name)):
+                    #delete
+                    if i == max_copy:
+                        print "delete"
+                        os.remove(os.path.join(path, file_name))
+                    #move
+                    else:
+                        new_file_name = "{0}{1}".format(name, i+1)
+                        os.rename(os.path.join(path, file_name), os.path.join(path, new_file_name))
+                i -= 1
+            #rotate last one
+            if max_copy:
+                os.rename(os.path.join(path, name), os.path.join(path, "{0}1".format(name)))
 
 def usage(exit=False):
     print
@@ -91,12 +110,13 @@ def usage(exit=False):
     print
     if exit:
         sys.exit(1)
-        
+
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 4:
         usage(True)
     logger = Logger(sys.argv[1], sys.argv[2], sys.argv[4:])
     logger.check_file()
     logger.rotate(sys.argv[3])
-    
-    
+
